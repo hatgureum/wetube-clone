c58d17855d0ef90840390feb5fa54abf50b6d82f
hatgureum <73741078+hatgureum@users.noreply.github.com>
2022-11-01 00:48:10 +0900

    ~#3.1

--- src/server.js
+++ src/server.js
@@ -1,18 +1,39 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("tiny");
+app.use(logger);
 
-const handleHome = (req, res) => {
-  return res.send("Hi");
-};
+// Router
+const globalRouter = express.Router();
 
-const handleHaha = (req, res) => {
-  return res.send("haha");
-};
-app.get("/", handleHome);
-app.get("/haha", handleHaha);
+const handleHome = (req, res) => res.send("Home");
+// use routers insted of app
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+
+const handleEdit = (req, res) => res.send("Edit user");
+// "/users" is included already in userRouter
+// so we don't need to write "/users/edit"
+userRouter.get("/edit", handleEdit);
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+app.use("/", globalRouter);
+
+/* Summary */
+// if someone is going to access "/videos", express goes in to videoRotuer
+// videoRouter has a route, which is the URL, "/watch"
+// => thanks to app.use, [router].get([URL]) works as "/router/URL"
 
 const handelListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}/ 🚀`);
