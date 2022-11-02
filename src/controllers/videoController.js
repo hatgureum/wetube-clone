8aba2fd968f586c137a1a487849ccb083a2b7b6b
hatgureum <73741078+hatgureum@users.noreply.github.com>
2022-11-03 01:10:19 +0900

    ~#6.1

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,16 @@
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
-export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+let videos = [
+  { title: "First", rating: 5, comments: 2, createdAt: "", views: 59, id: 1 },
+  { title: "Second", rating: 5, comments: 2, createdAt: "", views: 1, id: 2 },
+  { title: "Third", rating: 5, comments: 2, createdAt: "", views: 57, id: 3 },
+];
+
+export const trending = (req, res) =>
+  res.render("home", { pageTitle: "Home", videos });
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+};
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,11 +3,16 @@
 // videoRouter has a route, which is the URL, "/watch"
 // => thanks to app.use, [router].get([URL]) works as "/router/URL"
 import express from "express";
-import { see, edit, upload, deleteVideo } from "../controllers/videoController";
+import {
+  watch,
+  edit,
+  upload,
+  deleteVideo,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)", watch);
 videoRouter.get("/:id(\\d+)/edit", edit);
 videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 videoRouter.get("/upload", upload);
