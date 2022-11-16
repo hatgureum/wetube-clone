d295607262b41f1b55d3cac18d47fa1ad37e34f2
hatgureum <73741078+hatgureum@users.noreply.github.com>
2022-11-17 02:06:58 +0900

    ~#7.23

--- src/models/User.js
+++ src/models/User.js
@@ -3,8 +3,10 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
+  avatarUrl: String,
+  socialOnly: { type: Boolean, default: false },
   username: { type: String, required: true, unique: true },
-  password: { type: String, requiered: true },
+  password: { type: String },
   name: { type: String, requiered: true },
   location: String,
 });
