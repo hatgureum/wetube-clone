bacb61ef82d14bafaae0294fe8068bca95a9a03c
hatgureum <73741078+hatgureum@users.noreply.github.com>
2022-11-25 21:54:34 +0900

    #13.2

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,13 +1,36 @@
 const startBtn = document.getElementById("startBtn");
 const preview = document.getElementById("preview");
 
-const handleStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+let stream = null;
+
+const handleStop = () => {
+  startBtn.innerText = "Start Recording";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleStart);
+};
+
+const handleStart = () => {
+  startBtn.innerText = "Stop Recording";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+
+  const recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (event) => console.log(event);
+  recorder.start();
+  setTimeout(() => {
+    recorder.stop();
+  }, 5000);
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
     audio: true,
     video: { width: 200, height: 100 },
   });
   video.srcObject = stream;
   video.play();
 };
+
+init();
 
 startBtn.addEventListener("click", handleStart);
