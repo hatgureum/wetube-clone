28753ee6bec49f2794a1f01541322e505c0fce92
hatgureum <73741078+hatgureum@users.noreply.github.com>
2022-11-28 00:03:41 +0900

    Update init.js

--- src/init.js
+++ src/init.js
@@ -6,7 +6,7 @@ import "./models/Video";
 import "./models/User";
 import app from "./server";
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 const handelListening = () =>
   console.log(`✔ Server listening on port http://localhost:${PORT}/ 🚀`);
